9d6280afc6cf756deeaa57dd2d2bcd1206d64dc2
Greg Woodward <woodward@users.noreply.github.com>
2023-06-03 11:11:44 -0700

    Delete the WebGL smart cell (for now, at least)

--- lib/kino_three_js/application.ex
+++ lib/kino_three_js/application.ex
@@ -5,8 +5,6 @@ defmodule KinoThreeJS.Application do
 
   @impl true
   def start(_type, _args) do
-    Kino.SmartCell.register(KinoThreeJS.WebGLCell)
-
     children = []
     opts = [strategy: :one_for_one, name: KinoThreeJS.Supervisor]
     Supervisor.start_link(children, opts)
